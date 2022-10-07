b4338f62c396b6b376fe9fc9df40129faa023e36
Petrus Borges Machado <contatopetrusborges@gmail.com>
2022-10-07 16:16:47 -0300

    Delete src directory

--- src/components/Toast/ToastContainer/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  left: 50%;
-  z-index: 2;
-  bottom: 48px;
-  position: fixed;
-  transform: translateX(-50%);
-`
--- src/utils/toast.js
+++
@@ -1,8 +0,0 @@
-//lib
-import EventManager from "../lib/EventManager"
-
-export const toastEventManager = new EventManager()
-
-export default function toast({ type, text, duration }) {
-  toastEventManager.emit('addtoast', { text, type, duration })
-}
