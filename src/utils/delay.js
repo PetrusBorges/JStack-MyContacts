47d3aa2b6f600205997c53cef425881d6c97a017
Petrus Borges Machado <contatopetrusborges@gmail.com>
2022-10-12 01:07:31 -0300

    Delete src directory

--- src/utils/delay.js
+++
@@ -1,3 +0,0 @@
-export default function delay(ms = 1000) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
--- src/utils/toast.js
+++
@@ -1,8 +0,0 @@
-//libraries
-import EventManager from "../lib/EventManager"
-
-export const toastEventManager = new EventManager()
-
-export default function toast({ type, text, duration }) {
-  toastEventManager.emit('addtoast', { type, text, duration })
-}
